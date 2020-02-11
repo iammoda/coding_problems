c97e12e1290c5e42ea11f684b76c659a6e01e84b
iammoda <mo.gounder@yahoo.com>
2020-02-10 23:29:01 -0500

    coding problems

---
+++ dfs/depth_first_search.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        array.append(self.name)
+        depthFirstSearch(self.children)
