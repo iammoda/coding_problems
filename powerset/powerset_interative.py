4507625d2c65471e013b2c8c78ba3f531295484f
iammoda <mo.gounder@yahoo.com>
2020-04-21 15:30:34 -0400

    coding problems

---
+++ powerset/powerset_interative.py
@@ -0,0 +1,9 @@
+def powerset(array):
+    subsets = [[]]
+	for element in array:
+		for i in range(len(subsets)):
+			currentSubset = subsets[i]
+			subsets.append(currentSubset + [element])
+	return subsets
+    pass
+
