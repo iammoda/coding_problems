58033e957bc44b1801f2093c2265dc71e507f4a6
iammoda <mo.gounder@yahoo.com>
2020-07-27 23:17:29 -0400

    invert BST - iterative

---
+++ invert BST - iterative/invert BST - iterative.py
@@ -0,0 +1,13 @@
+def invertBinaryTree(tree):
+    queue = [tree]
+    while len(queue):
+        current = queue.pop(0)
+        if current is None:
+            continue
+        swapLeftandRight(current)
+        queue.append(current.left)
+        queue.append(current.right)
+
+
+def swapLeftandRight(tree):
+    tree.left, tree.right = tree.right, tree.left
